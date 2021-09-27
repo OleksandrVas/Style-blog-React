34560886fd19c32ea9b8f60e019316ea4953d18b
Oleksandr <sasha228123@gmail.com>
2021-09-27 18:21:48 +0300

    add footer and fixed him

--- src/container/Main/Main.js
+++ src/container/Main/Main.js
@@ -250,19 +250,8 @@ const Main = () => {
             </div>
         </div>
     </section>
-    <footer id="footer">
-        <div className="container">
-            <div className="row">
-                <div className="col-xs-10 contact-info-box">
-                    <div>© 2021 Betheme by <span><a href="">Muffing group</a></span> | All Rights Reserved | Powered by <span><a href="">WordPress</a></span>
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
     </>
-  );
+    );
 };
 
 export default Main;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./container/Header/Header";
 import Main from "./container/Main/Main";
-
+import Footer from "./container/Footer/Footer";
 
 const App = () => {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 };
