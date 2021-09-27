299461b1cc8b85e486829d72df004d0e119c2608
Oleksandr <sasha228123@gmail.com>
2021-09-27 18:07:58 +0300

    add new section -do you need style

--- src/container/Main/Main.js
+++ src/container/Main/Main.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "../Main/Main.css"
+import StylistHomeIcon from "./Img/stylist-homeicon.png"
+import StylistHomeCabluck from "./Img/stylist-home-cabluck.png"
+import StylistHomeShoe from "./Img/stylist-home-shoe.png"
 const Main = () => {
   return (
     <>
@@ -79,6 +82,61 @@ const Main = () => {
                 </div>
                 <div className="col-xs-4 without-padding">
                     <div className="third-bgc-article"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="do-you-need-style">
+        <div className="container">
+            <div className="row">
+                <div className="col-xs-12 style-article">
+                    <div className="col-xs-6">
+                        <h2 className="about-who third-style">Do you need a stylist?</h2>
+                    </div>
+                    <div className="col-xs-6">
+                        <div className="button-article third-button-article ">
+                            <a href="">contact me today</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-xs-12 bgc-fourth-article-bgc">
+                    <div className="col-xs-6">
+                        <div className="box-style">
+                            <h2>03</h2>
+                            <h3>Take a close look
+                                at my services</h3>
+                            <div className="icon-style">
+                                <div className="style-by-icon">
+                                    <div className="col-xs-4">
+                                        <div className="style-icon-box"><img src={StylistHomeIcon} alt="" /></div>
+                                    </div>
+                                    <div className="col-xs-8">
+                                        <div className="box-info-style">
+                                        Duis dignissim mi ut laoreet mollis. Nunc id tellus finibus, eleifend mi vel, maximus justo.
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="style-by-icon">
+                                    <div className="col-xs-4">
+                                        <div className="style-icon-box second-icon-style"> <img src={StylistHomeCabluck} alt="" /></div>
+                                    </div>
+                                    <div className="col-xs-8">
+                                        <div className="box-info-style">
+                                            Nam massa erat, aliquet a rutrum eu, sagittis ac nibh. Pellentesque velit dolor, suscipit in ligula a, suscipit rhoncus dui.
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="button-article style-button">
+                                <a href="">SEE ALL SERVICES</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xs-6 position-style">
+                        <div >
+                            <img src={StylistHomeShoe} alt="" height="400px" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
