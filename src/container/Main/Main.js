21cc6448bac22d2cf960836eee4b69df9daf5788
Oleksandr <sasha228123@gmail.com>
2021-09-29 17:36:45 +0300

    created FashionGalleries  and export to App

--- src/container/Main/Main.js
+++ src/container/Main/Main.js
@@ -5,6 +5,7 @@ import StylistHomeCabluck from "./Img/stylist-home-cabluck.png";
 import StylistHomeShoe from "./Img/stylist-home-shoe.png";
 import ButtonVievMoreInfo from "../../components/MainComp/MainButtonVievMoreInfo";
 import ImStylistFirst from "../../components/MainComp/ImStylistFirst";
+import FeshionGalleries from "../../components/MainComp/FashionGalleries";
 const Main = () => {
   return (
     <>
@@ -17,59 +18,7 @@ const Main = () => {
   );
 };
 
-const FeshionGalleries = () => {
-    return (
-<>
-    <section id="fashion-galleries">
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12">
-            <div className="second-article-img">
-              <div className="col-xs-6">
-                <div></div>
-              </div>
-              <div className="col-xs-6">
-                <div className="box-second-article">
-                  <div className="col-xs-12">
-                    <h2 className="number-of-article">02</h2>
-                  </div>
-                  <div className="col-xs-12">
-                    <div className="fashion-article">Fashion galleries</div>
-                  </div>
-                  <div className="col-xs-12">
-                    <h2 className="about-fashion">
-                      Duis dignissim mi ut laoreet mollis. Nunc id tellus
-                      finibus, eleifend mi vel, maximus justo. Maecenas mi
-                      tortor, pellentesque a aliquam ut, fringilla eleifend
-                      lectus. Maecenas ultrices tellus sit amet sem placerat
-                      tempor.
-                    </h2>
-                  </div>
-                  <div className="col-xs-12">
-                    <div className="button-article      second">
-                      <a href="">see the gallery</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-xs-4 without-padding">
-            <div className="first-bgc-article "></div>
-          </div>
-          <div className="col-xs-4 without-padding">
-            <div className="second-bgc-article"></div>
-          </div>
-          <div className="col-xs-4 without-padding">
-            <div className="third-bgc-article"></div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </>
-    )
-  
-};
+
 
 const DoYouNeedStyle = () => {
   return (
